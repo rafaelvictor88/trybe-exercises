5c2e7481ac21bb6adf6c542c219142e2924df42b
Rafael Victor <rafavictorgs@gmail.com>
2021-05-19 18:57:57 -0300

    Resolve o exercício 3 do bloco 4.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 51;
+let b = 60;
+let c = 25;
+
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else if (c > a && c > b) {
+  console.log(c);
+}
