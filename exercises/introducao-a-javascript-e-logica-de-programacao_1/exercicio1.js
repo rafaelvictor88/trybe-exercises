528e50dcc98b1f5cd816489671ecc3b4bc113735
Rafael Victor <rafavictorgs@gmail.com>
2021-05-19 18:40:07 -0300

    Resolve exercício 1 do Bloco 4.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+
+let sum = console.log(a + b);
+let subtraction = console.log(a - b);
+let product = console.log(a * b);
+let quotient = console.log(a / b);
+let rest = console.log(a % b);
