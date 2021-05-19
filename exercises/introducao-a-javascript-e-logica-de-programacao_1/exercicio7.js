fa25001976edd01bd7c041ae3633092dad5e5445
Rafael Victor <rafavictorgs@gmail.com>
2021-05-19 20:08:32 -0300

    Refaz de completa o exercício 7 do bloco 4.1

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
@@ -1,16 +1,23 @@
-let nota = 45;
+let nota = -5;
 
-if (nota >= 90) {
+if (nota >= 90 && nota <= 100) {
   console.log("Parabéns, Excelente!!! Você recebeu 'A'. Continue assim.");
-} else if (nota >= 80) {
+} 
+else if (nota >= 80 && nota < 90) {
   console.log("Parabéns, Muito Bom!!! Você recebeu 'B'. Mas ainda pode melhorar.");
-} else if (nota >= 70) {
+} 
+else if (nota >= 70 && nota < 80) {
   console.log("Atenção!!! Você recebeu 'C'. Com mais esforço e dedicação podemos subir essa nota.");
-} else if (nota >= 60) {
+} 
+else if (nota >= 60 && nota < 70) {
   console.log("Cuidado, Zona Perigosa. Você recebeu 'D'. Precisa estudar mais pra não ser reprovado.");
-} else if (nota >= 50) {
+} 
+else if (nota >= 50 && nota < 60) {
   console.log("ALERTA VERMELHO. Você recebeu 'E'. Você não foi reprovado dessa vez, mas se não tomar cuidado poderá ser em breve.");
-} else if (nota < 50) {
+} 
+else if (nota < 50 && nota >= 0) {
   console.log("Que pena, infelizmente você não atingiu o conceito mínimo para ser aprovado. Você recebeu 'F', vamos trabalhar mais e melhor para que esse cenário mude da próxima vez.");
+} else if (nota < 0 || nota > 100) {
+  console.log("Valor de nota inválido. Programa encerrado!");
 }
 
