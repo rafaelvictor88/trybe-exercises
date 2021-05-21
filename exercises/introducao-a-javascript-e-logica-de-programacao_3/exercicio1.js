221892dd5c3f858cd81b64f0f69ef91673ed08cc
Rafael Victor <rafavictorgs@gmail.com>
2021-05-21 18:46:03 -0300

    Resolve exercício 1 do bloco 4.3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -0,0 +1,43 @@
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for(let index = 0; index < n; index += 1){
+  inputLine = inputLine + symbol
+}
+
+
+for(let index = 0; index < 5; index += 1){
+  console.log(inputLine);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for(let index = 0; index < n; index += 1){
+//   inputLine += symbol 
+// }
+
+// for(let index = 0; index < n; index += 1){
+//   console.log(inputLine);
+// }
