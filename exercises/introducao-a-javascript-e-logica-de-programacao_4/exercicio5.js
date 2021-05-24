d5f0b36bf5b25637447c23676d06eeba8c0f4133
Rafael Victor <rafavictorgs@gmail.com>
2021-05-24 18:20:34 -0300

    Resolve o exercício 5 do bloco 4.4

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio5.js
@@ -0,0 +1,19 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info['recorrente'] = 'Sim'
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+};
+
+info2['recorrente'] = 'Sim'
+
+for(let key in info && info2){
+  console.log(info[key], 'e', info2[key]);
+};
