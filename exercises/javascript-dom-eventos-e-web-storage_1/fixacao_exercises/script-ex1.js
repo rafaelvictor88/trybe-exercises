82f44f33a8cf6b3178a30bcfae600a4b27dc2aeb
Rafael Victor <rafavictorgs@gmail.com>
2021-05-26 19:37:17 -0300

    Resolve parte 3 (BGColor-white) do exercicio 1 do bloco 5.1

--- exercises/javascript-dom-eventos-e-web-storage_1/fixacao_exercises/script-ex1.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/fixacao_exercises/script-ex1.js
@@ -1,3 +1,5 @@
 document.getElementsByTagName("p")[1].innerText = 'Formado em Desenvolvimento Web pela Trybe e trabalhando na área de tecnologia.';
 
 document.querySelector(".main-content").style.backgroundColor = 'rgb(76,164,109)';
+
+document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
