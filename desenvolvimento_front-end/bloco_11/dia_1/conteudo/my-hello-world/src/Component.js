712ceb96ee69217a6144c71cc56683b6c6639247
Rafael Victor <rafavictorgs@gmail.com>
2021-07-21 20:07:47 -0300

    Criando meu primeiro Hello, World com React

---
+++ desenvolvimento_front-end/bloco_11/dia_1/conteudo/my-hello-world/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MyName extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rafael Victor Guimarães dos Santos</h1>
+        <p>Tenho 32, quase 33 anos. Sou Carioca e fui criado no interior do Estado RJ!</p>
+      </div>
+    )
+  }
+};
+
+export default MyName;
