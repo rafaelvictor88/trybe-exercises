067ec1a7c5c639d861404ae3a66a6687444d0e19
Rafael Victor <rafavictorgs@gmail.com>
2021-12-08 17:52:52 -0300

    Course bloco 26.4

---
+++ desenvolvimento_back-end/bloco_26/course/dia_4/hello-express/index.js
@@ -0,0 +1,33 @@
+// Para fixar:
+
+// 1- Crie uma array drinks com os seguintes objetos dentro dela e uma rota GET /drinks que retorna a lista de bebidas.
+const express = require('express');
+
+const app = express();
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/drinks', (req, res) => {
+  res.json(drinks);
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação escutando na porta 3001!');
+});
