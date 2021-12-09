abf14b00861c9570560d8710b16cfb01afb777a5
Rafael Victor <rafavictorgs@gmail.com>
2021-12-08 22:25:52 -0300

    Course bloco 26.4

--- desenvolvimento_back-end/bloco_26/course/dia_4/hello-express/index.js
+++ desenvolvimento_back-end/bloco_26/course/dia_4/hello-express/index.js
@@ -1,9 +1,10 @@
 // Para fixar:
 
-// 1- Crie uma array drinks com os seguintes objetos dentro dela e uma rota GET /drinks que retorna a lista de bebidas.
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -12,7 +13,34 @@ const recipes = [
 ];
 
 app.get('/recipes', function (req, res) {
-  res.json(recipes);
+  const organizado = recipes.sort(function(a, b) {return a.name.localeCompare(b.name)});
+  res.json(organizado);
+});
+
+app.get('/recipes/search', function (req, res) {
+  const { name, maxPrice, minPrice } = req.query;
+  const filteredRecipes = recipes
+    .filter((r) => r.name.includes(name)
+    && r.price < parseInt(maxPrice)
+    && r.price >= parseInt(minPrice));
+
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  // console.log(req);
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+
+  res.status(200).json(recipe);
+});
+
+app.post('/recipes', function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price});
+  res.status(201).json({ message: 'Recipe created successfully!'});
 });
 
 const drinks = [
@@ -25,7 +53,26 @@ const drinks = [
 ];
 
 app.get('/drinks', (req, res) => {
-  res.json(drinks);
+  const organizado = drinks.sort(function(a, b) {return a.name.localeCompare(b.name)});
+  res.json(organizado);
+});
+
+app.get('/drinks/search', function(req, res) {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((d) => d.name.toLowerCase().includes(name));
+
+  if (!filteredDrinks) return res.status(404).json({ message: 'No such drink' });
+
+  res.status(200).json(filteredDrinks);
+});
+
+app.get('/drinks/:id', function(req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === parseInt(id, 10));
+
+  if (!drink) return res.status(404).json({ message: 'drink not found' });
+
+  res.status(200).json(drink);
 });
 
 app.listen(3001, () => {
