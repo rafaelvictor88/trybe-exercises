3d3b10674935f59d39859cd2f4b63781a162c5b9
Rafael Victor <rafavictorgs@gmail.com>
2021-12-31 07:36:57 -0300

    Exercicios 26.2 revisando - Ex2

--- desenvolvimento_back-end/bloco_26/exercises/dia_2_revisando/ex1.js
+++ desenvolvimento_back-end/bloco_26/exercises/dia_2_revisando/ex1.js
@@ -7,17 +7,19 @@ const myNumbers = (num1, num2, num3) => {
   });
   return myPromise;
 }
-// Chamada para o valor "correto";
-myNumbers(10, 20, 30)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err.message));
+// // Chamada para o valor "correto";
+// myNumbers(10, 20, 30)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err.message));
 
-// Chamada para o caso de valor muito baixo;
-myNumbers(1, 2, 3)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err.message));
+// // Chamada para o caso de valor muito baixo;
+// myNumbers(1, 2, 3)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err.message));
 
-// Chamada para o caso de erro;
-myNumbers('a', 2, 3)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err.message));
+// // Chamada para o caso de erro;
+// myNumbers('a', 2, 3)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err.message));
+
+module.exports = myNumbers;
